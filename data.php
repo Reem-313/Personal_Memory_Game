eed7f121062d8709436108554f54527c48fa8200
Reem Khider <74662297+Reem-313@users.noreply.github.com>
2023-04-27 22:05:17 +0100

    delete image from the database

--- data.php
+++ data.php
@@ -19,6 +19,8 @@ else{
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=fit">
+                <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+
             <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Lexend:wght@300&display=swap" rel="stylesheet">
@@ -28,49 +30,49 @@ else{
         
     </head>
     <body>
-        <table border = 1 cellspacing = 0 cellpadding= 10>
-            <tr>
-                <td> NO. </td>
-                <td>Decription</td>
-                <td>Image</td>
-            </tr>
-                <?php
-                $i = 1;
-                $rows =mysqli_query($conn, "SELECT * FROM image WHERE userIdFK= $id");
-                ?>
-                <?php foreach ($rows as $row) : ?>
-            <tr>
-                <td> <?php echo $i++; ?></td>
-                <td><?php echo $row['Description']; ?></td>
-                <td> <img src= "img/<?php echo $row['imagePath']; ?>" alt ="<?php echo $row['Description']; ?>" width =200></td>
-                <td><form method="post" action="">
-                    <input type="submit" name="submit" value="Delete" onclick="delete_image(<?php echo $row['imageId']; ?>)">
-                    </td></form>
-            </tr>
-            <?php endforeach; ?>
-        </table>
-        <br>
-        <h3><a href="upload.php"> Please upload more photos here </a><br></h3>
-        <script>
-            function delete_image(imgid)
-            {
-                console.log(imgid);
-                 <?php
-                 if(isset($_POST['submit']))
-{ 
-        $imgID = $_POST['imgid'];
+        <div class="table-responsive">          
+            <table class="table table-bordered table-sm" border = 1 cellspacing = 0 cellpadding= 10>
+                <tr>
+                    <td> NO. </td>
+                    <td>Description</td>
+                    <td>Image</td>
+                </tr>
+                    <?php
+                    $i = 1;
+                    $rows =mysqli_query($conn, "SELECT * FROM image WHERE userIdFK= $id");
+                    ?>
+                    <?php foreach ($rows as $row) : ?>
+                <tr>
+                    <td> <?php echo $i++; ?></td>
+                    <td><?php echo $row['Description']; ?></td>
+                    <td> <img src= "img/<?php echo $row['imagePath']; ?>" alt ="<?php echo $row['Description']; ?>" width =200></td>
+                    <td><form method="post" action="deleteImg.php">
+                        <input type="hidden" value="<?php echo $row['imageId'];?>" name="imgid">
+                        <input type="submit" name="submit" value="Delete">
+                        </td></form>
+                </tr>
+                <?php endforeach; ?>
+            </table>
+        </div>
+                <br>
 
-                $query=  "DELETE FROM image WHERE imageId = $imgID";
-                 if(mysqli_query($conn, "DELETE FROM image WHERE imageId = $imgID"))
-                    {
-                          echo
-                    "<script>
-                     alert('Image successfully deleted!');
-                    </script>";
-                    }
-}
-                ?>
-                }
-        </script>
+         <div class="container card border-primary mb-3">
+                             <br>
+
+                    <div class="form-group mx-sm-3 mb-2">
+                        <button onClick="location.href='preplaying.php'" type="button" class="btn btn-outline-primary btn-lg">Start Playing</button>
+                    </div>
+                    
+                    <div class="form-group mx-sm-3 mb-2">
+                        <button onClick="location.href='welcome.php'" type="button" class="btn btn-outline-primary btn-lg">Go back to the preivous page</button>
+                    </div>
+                    <div class="form-group mx-sm-3 mb-2">
+                        <button onClick="location.href='upload.php'" type="button" class="btn btn-outline-primary btn-lg">Please upload your photos here</button>
+                    </div>
+                    <div class="form-group mx-sm-3 mb-2">
+                        <a href="logout.php"> Logout </a>
+                    </div>
+        </div>
+
     </body>
 </html>
